e6617eba85bc579ba685d187fac9afde92b7e358
Ali AlRasheed <ali.alrasheed@outlook.com>
2021-06-20 02:02:31 +0300

    Setting up the graphics

--- core/src/main/kotlin/com/github/imbackt/darkmatter/DarkMatter.kt
+++ core/src/main/kotlin/com/github/imbackt/darkmatter/DarkMatter.kt
@@ -2,21 +2,23 @@ package com.github.imbackt.darkmatter
 
 import com.badlogic.gdx.Application.LOG_DEBUG
 import com.badlogic.gdx.Gdx
-import com.github.imbackt.darkmatter.screen.FirstScreen
-import com.github.imbackt.darkmatter.screen.SecondScreen
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.github.imbackt.darkmatter.screen.DarkMatterScreen
+import com.github.imbackt.darkmatter.screen.GameScreen
 import ktx.app.KtxGame
-import ktx.app.KtxScreen
 import ktx.log.debug
 import ktx.log.logger
 
 private val LOG = logger<DarkMatter>()
 
-class DarkMatter : KtxGame<KtxScreen>() {
+class DarkMatter : KtxGame<DarkMatterScreen>() {
+    val batch : Batch by lazy { SpriteBatch() }
+
     override fun create() {
         Gdx.app.logLevel = LOG_DEBUG
         LOG.debug { "Create game instance" }
-        addScreen(FirstScreen(this))
-        addScreen(SecondScreen(this))
-        setScreen<FirstScreen>()
+        addScreen(GameScreen(this))
+        setScreen<GameScreen>()
     }
 }
--- core/src/main/kotlin/com/github/imbackt/darkmatter/screen/DarkMatterScreen.kt
+++ core/src/main/kotlin/com/github/imbackt/darkmatter/screen/DarkMatterScreen.kt
@@ -1,6 +1,10 @@
 package com.github.imbackt.darkmatter.screen
 
+import com.badlogic.gdx.graphics.g2d.Batch
 import com.github.imbackt.darkmatter.DarkMatter
 import ktx.app.KtxScreen
 
-abstract class DarkMatterScreen(val game: DarkMatter) : KtxScreen
+abstract class DarkMatterScreen(
+    val game : DarkMatter,
+    val batch : Batch = game.batch
+    ) : KtxScreen
