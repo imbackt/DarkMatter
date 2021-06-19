c8908046bb0cde0289e3b277a84d1ddf6d5ccc7f
Ali AlRasheed <ali.alrasheed@outlook.com>
2021-06-19 12:23:00 +0300

    Setting Up a logger with test screens

--- core/src/main/kotlin/com/github/imbackt/darkmatter/DarkMatter.kt
+++ core/src/main/kotlin/com/github/imbackt/darkmatter/DarkMatter.kt
@@ -1,9 +1,22 @@
 package com.github.imbackt.darkmatter
 
-import com.badlogic.gdx.Game
+import com.badlogic.gdx.Application.LOG_DEBUG
+import com.badlogic.gdx.Gdx
+import com.github.imbackt.darkmatter.screen.FirstScreen
+import com.github.imbackt.darkmatter.screen.SecondScreen
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
+import ktx.log.debug
+import ktx.log.logger
 
-class DarkMatter : Game() {
+private val LOG = logger<DarkMatter>()
+
+class DarkMatter : KtxGame<KtxScreen>() {
     override fun create() {
-        setScreen(FirstScreen())
+        Gdx.app.logLevel = LOG_DEBUG
+        LOG.debug { "Create game instance" }
+        addScreen(FirstScreen(this))
+        addScreen(SecondScreen(this))
+        setScreen<FirstScreen>()
     }
 }
---
+++ core/src/main/kotlin/com/github/imbackt/darkmatter/screen/DarkMatterScreen.kt
@@ -0,0 +1,6 @@
+package com.github.imbackt.darkmatter.screen
+
+import com.github.imbackt.darkmatter.DarkMatter
+import ktx.app.KtxScreen
+
+abstract class DarkMatterScreen(val game: DarkMatter) : KtxScreen
