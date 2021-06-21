081a0f411207e22f6a3cda3305c1ffde3d139a57
Ali AlRasheed <ali.alrasheed@outlook.com>
2021-06-21 22:51:52 +0300

    Implementing render system

--- core/src/main/kotlin/com/github/imbackt/darkmatter/DarkMatter.kt
+++ core/src/main/kotlin/com/github/imbackt/darkmatter/DarkMatter.kt
@@ -6,6 +6,8 @@ import com.badlogic.gdx.Application.LOG_DEBUG
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.viewport.FitViewport
+import com.github.imbackt.darkmatter.ecs.system.RenderSystem
 import com.github.imbackt.darkmatter.screen.DarkMatterScreen
 import com.github.imbackt.darkmatter.screen.GameScreen
 import ktx.app.KtxGame
@@ -16,8 +18,13 @@ const val UNIT_SCALE = 1 / 16f
 private val LOG = logger<DarkMatter>()
 
 class DarkMatter : KtxGame<DarkMatterScreen>() {
+    val gameViewport = FitViewport(9f, 16f)
     val batch: Batch by lazy { SpriteBatch() }
-    val engine: Engine by lazy { PooledEngine() }
+    val engine: Engine by lazy {
+        PooledEngine().apply {
+            addSystem(RenderSystem(batch, gameViewport))
+        }
+    }
 
     override fun create() {
         Gdx.app.logLevel = LOG_DEBUG
--- core/src/main/kotlin/com/github/imbackt/darkmatter/screen/DarkMatterScreen.kt
+++ core/src/main/kotlin/com/github/imbackt/darkmatter/screen/DarkMatterScreen.kt
@@ -2,11 +2,17 @@ package com.github.imbackt.darkmatter.screen
 
 import com.badlogic.ashley.core.Engine
 import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.utils.viewport.Viewport
 import com.github.imbackt.darkmatter.DarkMatter
 import ktx.app.KtxScreen
 
 abstract class DarkMatterScreen(
-    val game : DarkMatter,
-    val batch : Batch = game.batch,
+    val game: DarkMatter,
+    val batch: Batch = game.batch,
+    val gameViewport: Viewport = game.gameViewport,
     val engine: Engine = game.engine
-    ) : KtxScreen
+) : KtxScreen {
+    override fun resize(width: Int, height: Int) {
+        gameViewport.update(width, height, true)
+    }
+}
